17007535012007d86cb32da63b64b571c268bbdf
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-14 13:15:21 +0200

    add post component

---
+++ git-practice/src/Post/Post.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Post() {
+    return (
+        <div>
+            <p>
+                subject
+            </p>
+            <p>
+                article
+            </p>
+            <p>
+                comments
+            </p>
+        </div>
+    );
+}
+
+export default Post;
